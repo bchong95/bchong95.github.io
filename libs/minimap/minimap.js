7aa04901cc71a97dc78c63157999b32b685040fb
Brandon Chong <bschong2@illinois.edu>
2016-09-23 17:43:27 -0500

    taller mini map?

--- libs/minimap/minimap.js
+++ libs/minimap/minimap.js
@@ -32,7 +32,7 @@ SOFTWARE.
         var shown = true;
 
         var defaults = {
-            heightRatio : 0.6,
+            heightRatio : 0.9,
             widthRatio : 0.1,
             offsetHeightRatio : 0.035,
             offsetWidthRatio : 0.035,
